a38c8b3fcaeb3f510ecc694de83581bf24474fec
rezjsh <149146755+rezjsh@users.noreply.github.com>
2024-03-31 11:21:26 +0330

    install required pacages

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # NLP-TextClassification-HateOrLove
+
+
+## How to run?
+
+```bash
+conda create -n hate python=3.12 -y
+```
+
+```bash
+conda activate hate
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='hate-speech-classification',
+    version='0.0.1',
+    author='rezajsh',
+    author_email='example@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
